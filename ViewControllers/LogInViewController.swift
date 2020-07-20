11f9929de6b4b39258429952972b7de15dd406b5
dtabaja <dtabaja@gmail.com>
2020-07-20 22:21:34 +0100

    1#

---
+++ ViewControllers/LogInViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LogInViewController.swift
+//  Shift_Share
+//
+//  Created by user167528 on 7/20/20.
+//  Copyright © 2020 user167528. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+ 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+ 
+    
+    @IBAction func logInButtonTapped(_ sender: Any) {
+    }
+    
+}
---
+++ ViewControllers/SignUpViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SignUpViewController.swift
+//  Shift_Share
+//
+//  Created by user167528 on 7/20/20.
+//  Copyright © 2020 user167528. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     setUpElements()
+           }
+           
+           func setUpElements() {
+           
+               // Hide the error label
+               errorLabel.alpha = 0
+           
+               // Style the elements
+               Utilities.styleTextField(firstNameTextField)
+               Utilities.styleTextField(lastNameTextField)
+               Utilities.styleTextField(emailTextField)
+               Utilities.styleTextField(passwordTextField)
+               Utilities.styleFilledButton(signUpButton)
+           }
+    
+    // Check the fields and validate that the data is correct. If everything is correct, this method returns nil. Otherwise, it returns the error message
+    func validateFields() -> String? {
+        
+        // Check that all fields are filled in
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        // Check if the password is secure
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            // Password isn't secure enough
+            return "Please make sure your password is at least 8 characters, contains a special character and a number."
+        }
+        
+        return nil
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
+    
+}
