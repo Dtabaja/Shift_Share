f82310ad64d1c70fcf07b1d70f9871d78f1fe68a
dtabaja <dtabaja@gmail.com>
2020-07-22 10:24:17 +0100

    3#

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Shift-Share' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Shift-Share
+
+pod 'Firebase/Analytics'
+pod 'Firebase/Auth'
+pod 'Firebase/Core'
+pod 'Firebase/Firestore'
+pod 'FSCalendar'
+
+end
---
+++ Shift_Share/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Shift_Share
+//
+//  Created by user167528 on 7/20/20.
+//  Copyright © 2020 user167528. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
+    
+    @IBOutlet weak var signUpButton: UIButton!
+
+    @IBOutlet weak var logInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+                setUpElements()
+            }
+            
+            override func viewWillAppear(_ animated: Bool) {
+                
+                // Set up video in the background
+                setUpVideo()
+            }
+            
+            func setUpElements() {
+                
+                Utilities.styleFilledButton(signUpButton)
+                Utilities.styleHollowButton(logInButton)
+                
+            }
+            
+            func setUpVideo() {
+                
+                // Get the path to the resource in the bundle
+                let bundlePath = Bundle.main.path(forResource: "loginbg", ofType: "mp4")
+                
+                guard bundlePath != nil else {
+                    return
+                }
+                
+                // Create a URL from it
+                let url = URL(fileURLWithPath: bundlePath!)
+                
+                // Create the video player item
+                let item = AVPlayerItem(url: url)
+                
+                // Create the player
+                videoPlayer = AVPlayer(playerItem: item)
+                
+                // Create the layer
+                videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+                
+                // Adjust the size and frame
+                videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height/1.5)
+                
+                view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+                
+                // Add it to the view and play it
+                videoPlayer?.playImmediately(atRate: 0.3)
+            }
+
+
+        }
